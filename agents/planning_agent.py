90ebfb7f235b1214e1de122c04b0788c65e4b337
Gaijutsu <jasper.koenig.22@ucl.ac.uk>
2026-02-21 19:57:08 +0000

    Fixed Crew tracing

--- agents/planning_agent.py
+++ agents/planning_agent.py
@@ -1009,6 +1009,9 @@ class TripPlanner:
             tasks=tasks,
             process=Process.sequential,
             verbose=VERBOSE,
+            tracing=TRACING,
+            memory=True,
+            cache=True,
         )
 
         import threading
